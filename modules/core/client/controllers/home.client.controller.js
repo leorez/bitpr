bb263fbbd2c4200ac8e6f5b2cb123f134b5e1093
lerorez <noruya@gmail.com>
2016-05-19 18:11:46 +0900

    검색시 기사 공유하기, 홈페이지에 올리기

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,17 +5,88 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController($scope, $http, Authentication, ngProgressFactory, $stateParams) {
+  function HomeController($scope, $location, DisplayedArticles, clipboard, $mdDialog, $http, Authentication, ngProgressFactory, $stateParams) {
     var vm = this;
 
     // This provides Authentication context.
     $scope.authentication = Authentication;
     $scope.resultArticles = [];
     $scope.progressbar = ngProgressFactory.createInstance();
+    $scope.selected = [];
 
-    // for test -------------------------------
-    $scope.resultArticles = [];
-    // ----------------------------------------
+    $scope.toggle = function (item, list) {
+      var idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+      } else {
+        list.push(item);
+      }
+
+      console.log(JSON.stringify(list));
+    };
+
+    $scope.exists = function (item, list) {
+      return list.indexOf(item) > -1;
+    };
+
+    $scope.displayArticleToHomepage = function () {
+      // 선택된 기사 홈페이지에 올리기
+      $scope.success = $scope.error = null;
+
+      if (Authentication.user) {
+        var selected = $scope.selected;
+
+        angular.forEach(selected, function (item) {
+          var article = new DisplayedArticles({
+            title: item.title,
+            summary: item.summary,
+            media: item.media,
+            url: item.url,
+            articleAt: item.article_at
+          });
+
+          console.log('article: ' + JSON.stringify(article));
+
+          article.$save(function (response) {
+            console.log('success:' + response);
+          }, function (errorRespose) {
+            $scope.error = errorRespose.data.message;
+            console.log('failed: ' + $scope.error);
+            console.log(JSON.stringify(errorRespose));
+          });
+        });
+      }
+    };
+
+    $scope.closeDialog = function () {
+      $mdDialog.hide();
+    };
+
+    $scope.shareArticle = function ($event) {
+      // 선택된 기사 공유하기
+      // : 제목과 링크로 구성된 문자열을 만들어 클립보드로 복사한다.
+      if (Authentication.user) {
+        var selected = $scope.selected;
+        var text = '';
+        angular.forEach(selected, function (item) {
+          text += '<p>' + item.title + ' <a href="' + item.url + '">더보기</a></p>';
+        });
+        console.log(text);
+
+        clipboard.copyText(text);
+
+        var alert = $mdDialog.alert()
+          .title('기사링크가 복사되었습니다.')
+          .htmlContent('<md-content>' + text + '</md-content>')
+          .ok('닫기');
+
+        $mdDialog
+          .show(alert)
+          .finally(function () {
+            alert = undefined;
+          });
+      }
+    };
 
     $scope.goSearch = function () {
       console.log($stateParams.corpCode);

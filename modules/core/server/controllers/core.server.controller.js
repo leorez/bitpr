9ed1956c96ce804d600d62e3dcc6c1b61669af14
lerorez <noruya@gmail.com>
2016-05-25 15:30:10 +0900

    네이버 연동

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -21,7 +21,7 @@ exports.renderIndex = function (req, res) {
   var safeUserObject = null;
   if (req.user) {
     safeUserObject = {
-      displayName: req.user.displayName,
+      displayName: validator.escape(req.user.displayName),
       provider: validator.escape(req.user.provider),
       created: req.user.created.toString(),
       roles: req.user.roles,
--- modules/users/server/config/strategies/naver.js
+++ modules/users/server/config/strategies/naver.js
@@ -21,10 +21,11 @@
 
       // Create the user OAuth profile
       var providerUserProfile = {
-        displayName: profile.username,
+        displayName: profile.displayName,
         email: profile.emails ? profile.emails[0].value : undefined,
         username: profile.id || generateUsername(profile),
         provider: 'naver',
+        profileImageURL: providerData.profile_image,
         providerIdentifierField: 'id',
         providerData: providerData
       };

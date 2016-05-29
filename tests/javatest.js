a9de0e1684e9a9b728bd9ed98ab4dfab8787c7bc
lerorez <noruya@gmail.com>
2016-05-29 13:36:06 +0900

    doctodocx test

--- tests/javatest.js
+++ tests/javatest.js
@@ -1,4 +1,20 @@
 var java = require('java');
-var javaLangSystem = java.import('java.lang.System');
-javaLangSystem.out.printlnSync('Hello Wolrd');
+// java.classpath.push('jodconverter-2.2.2.jar');
+java.classpath.push('./jodconverter-2.2.2');
+//java.classpath.push("commons-io.jar");
 
+var File = java.import('java.io.File');
+var inputFile = new File('test.doc');
+var outputFile = new File('out.docx');
+
+java.import("java.util.ArrayList");
+
+var SocketOpenOfficeConnection = java.newInstanceSync('SocketOpenOfficeConnection');
+// var SocketOpenOfficeConnection = java.import('com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection');
+// var connection = new SocketOpenOfficeConnection(8100);
+// connection.connect();
+
+// var OpenOfficeDocumentConverter = java.newInstanceSync('com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter');
+// var converter = new OpenOfficeDocumentConverter(connection);
+// converter.convert(inputFile, outputFile);
+// connection.disconnect();
